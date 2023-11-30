f712b05b72ff31e2d072d79069f81ed2e61cee26
ying <leftleft0813@gmail.com>
2023-11-30 23:22:25 +0800

    Add:index.js-gratitube benefit design

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,32 @@ import styles from "../styles/page.module.css";
 import Nav from "../components/Nav/Nav";
 import Header from "../components/Header/Header";
 import Member from "@/components/Member/Member";
+import GratitudeBenefit from "@/components/GratitudeBenefit/GratitudeBenefit";
 import { useRouter } from "next/router";
 
 export default function Home() {
   const router = useRouter();
   const showMember = router.query.showMember === "true";
   return (
-    <div className={styles.homeLayout}>
-      <div className={styles.headerLayout}>
-        <Header />
-        <Nav />
+    <>
+      <div className={styles.homeLayout}>
+        <div className={styles.headerLayout}>
+          <Header />
+          <Nav />
+        </div>
+        {showMember && <Member />}
+        <img
+          className={styles.mouseCursor}
+          src="/images/mouse-cursor.png"
+          alt="mouse-cursor"
+          width={"40px"}
+          height={"40px"}
+        />
       </div>
-      {showMember && <Member />}
-    </div>
+
+      <div className={styles.middleLayout}>
+        <GratitudeBenefit />
+      </div>
+    </>
   );
 }
