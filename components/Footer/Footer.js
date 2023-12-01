9e018cd8f3bce800c1e2b418681356ca8fb6ab29
ying <leftleft0813@gmail.com>
2023-12-01 11:10:40 +0800

    Add Footer

---
+++ components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+export const Footer = () => {
+  return (
+    <div className={styles.footerWrapper}>
+      <p className={styles.footerText}>Copyright 2023 @ GratifyME</p>
+    </div>
+  );
+};
