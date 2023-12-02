332914590942c06a6a70622c0e0cb4dc58c7fb3b
ying <leftleft0813@gmail.com>
2023-12-02 23:43:51 +0800

    Add:Editor.js loading effect

--- components/Editor/Editor.js
+++ components/Editor/Editor.js
@@ -24,10 +24,10 @@ import AlertDialog from "../AlertDialog/AlertDialog";
 import Alert from "@mui/material/Alert";
 import AlertTitle from "@mui/material/AlertTitle";
 import Stack from "@mui/material/Stack";
-import { BeatLoader } from "react-loader-spinner";
+import LoadingOverlay from "./LoadingOverLay";
 
 // 讀取Firestore的資料
-function EditorInnerComponent({ date, uid, onEditorReady }) {
+function EditorInnerComponent({ date, uid, onEditorReady, setIsLoading }) {
   const [editor] = useLexicalComposerContext();
 
   useEffect(() => {
@@ -71,7 +71,7 @@ function EditorInnerComponent({ date, uid, onEditorReady }) {
     if (uid && date) {
       loadContent();
     }
-  }, [editor, date, uid, onEditorReady]);
+  }, [editor, date, uid, onEditorReady, setIsLoading]);
 
   return null;
 }
@@ -111,6 +111,7 @@ export default function Editor({ date }) {
 
   // 保存内容到Firebase
   const saveToFirebase = async (currentContent) => {
+    setIsLoading(true);
     if (uid && date) {
       try {
         const serializedState = JSON.stringify(currentContent);
@@ -128,57 +129,52 @@ export default function Editor({ date }) {
         }, 2200);
       } catch (error) {
         console.error("Error writing document:", error);
+      } finally {
+        setIsLoading(false);
       }
     }
   };
 
-  //刪除firebase storage & firestore
   const deleteToFirebase = async () => {
+    setIsLoading(true);
     const diaryEntryRef = doc(firebase.db, "users", uid, "diaryEntries", date);
     const diarySnapshot = await getDoc(diaryEntryRef);
 
-    if (diarySnapshot.exists()) {
-      const editorStateData = diarySnapshot.data().editorState;
-      let imageUrl;
+    try {
+      if (diarySnapshot.exists()) {
+        const editorStateData = diarySnapshot.data().editorState;
+        let imageUrl;
 
-      // 如果 editorStateData 是字符串，則解析 JSON 以提取圖片 URL
-      if (typeof editorStateData === "string") {
-        const editorState = JSON.parse(editorStateData);
-        imageUrl = editorState.root.children
-          .flatMap((child) => child.children || [])
-          .find((child) => child.type === "image")?.src;
-      }
+        // 如果 editorStateData 是字符串，則解析 JSON 以提取圖片 URL
+        if (typeof editorStateData === "string") {
+          const editorState = JSON.parse(editorStateData);
+          imageUrl = editorState.root.children
+            .flatMap((child) => child.children || [])
+            .find((child) => child.type === "image")?.src;
+        }
 
-      // 如果圖片 URL 存在，則提取 Storage 中的文件路徑
-      if (imageUrl) {
-        const imagePath = decodeURIComponent(
-          new URL(imageUrl).pathname.split("/o/")[1].split("?")[0]
-        );
-
-        // 創建指向 Storage 中圖片的引用
-        const imageRef = ref(getStorage(firebase.app), imagePath);
-
-        // 刪除 Storage 中的圖片
-        try {
+        // 如果圖片 URL 存在，則刪除 Storage 中的圖片
+        if (imageUrl) {
+          const imagePath = decodeURIComponent(
+            new URL(imageUrl).pathname.split("/o/")[1].split("?")[0]
+          );
+          const imageRef = ref(getStorage(firebase.app), imagePath);
           await deleteObject(imageRef);
           console.log("Image deleted successfully from Storage.");
-        } catch (error) {
-          console.error("Failed to delete image from Storage:", error);
-          return;
         }
-      }
 
-      // 刪除 Firestore 中的日記
-      try {
+        // 刪除 Firestore 中的日記條目
         await deleteDoc(diaryEntryRef);
         console.log("Diary entry deleted successfully from Firestore.");
-        setShowDialog(false);
-        router.reload();
-      } catch (error) {
-        console.error("Failed to delete diary entry from Firestore:", error);
+      } else {
+        console.log("Diary entry does not exist.");
       }
-    } else {
-      console.log("Diary entry does not exist.");
+    } catch (error) {
+      console.error("Failed to delete:", error);
+    } finally {
+      setShowDialog(false);
+      router.reload();
+      setIsLoading(false);
     }
   };
   // 處理對話框的確認操作
@@ -202,11 +198,7 @@ export default function Editor({ date }) {
 
   return (
     <LexicalComposer initialConfig={lexicalEditorConfig}>
-      {isLoading && (
-        <div className={styles.loader}>
-          <BeatLoader color="#36D7B7" />
-        </div>
-      )}
+      {isLoading && <LoadingOverlay />}
       <ToggleButton isChecked={isEditMode} onToggle={toggleEditMode} />
       {isEditMode ? (
         <>
@@ -263,6 +255,7 @@ export default function Editor({ date }) {
                 date={date}
                 uid={uid}
                 onEditorReady={handleEditorReady}
+                setIsLoading={setIsLoading}
               />
             )}
           </Box>
